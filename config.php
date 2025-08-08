cdefdf9d20aef85fa821e420c29c082067555343
Buianh20004 <buiphuonganh081224@gmail.com>
2025-08-08 08:21:22 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,16 +1,13 @@
 <?php
-$host = getenv('DB_HOST');
-$port = getenv('DB_PORT');
-$dbname = getenv('DB_NAME');
-$username = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
+$host = getenv("DB_HOST");
+$port = getenv("DB_PORT");
+$dbname = getenv("DB_NAME");
+$username = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
 
 $conn = new mysqli($host, $username, $password, $dbname, $port);
 
-//chec connection
-
-if($conn->connect_error){
-    die("ket noi taht bai".$conn->connect_error);
+if ($conn->connect_error) {
+    die("❌ Kết nối thất bại: " . $conn->connect_error);
 }
-
 ?>
